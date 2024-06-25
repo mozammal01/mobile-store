2fae35025af04c4b9306c63c34beb2e19b7ee0c1
Mozammal Haq <mozammalhaq01@gmail.com>
2024-06-26 00:15:32 +0600

    password check and login form done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,21 +1,35 @@
 import { Link } from "react-router-dom";
 
 const Login = () => {
+
+  const handleLogin = e => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const email = form.email.value;
+    const password = form.password.value;
+
+    const user = { email, password }
+
+    console.log(user);
+  }
+
   return (
     <div className="hero min-h-screen bg-slate-200">
       <div className="card w-full bg-white max-w-sm shrink-0 shadow-2xl">
-        <form className="card-body  ">
+        <form onSubmit={handleLogin} className="card-body  ">
           <div className="form-control">
             <label className="label">
               <span className="label-text text-black font-semibold">Email</span>
             </label>
-            <input type="email" placeholder="email" className="input input-bordered text-white" required />
+            <input type="email" name="email" placeholder="Email" className="input input-bordered text-white" required />
           </div>
           <div className="form-control">
             <label className="label">
               <span className="label-text text-black font-semibold">Password</span>
             </label>
-            <input type="password" placeholder="password" className="input input-bordered text-white" required />
+            <input type="password" name="password" placeholder="Password" className="input input-bordered text-white" required />
             <label className="label">
               <a href="#" className="label-text-alt link link-hover text-red-600 font-bold text-base">Forgot password?</a>
             </label>
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,22 +1,74 @@
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Register = () => {
+
+  const handleRegister = e => {
+    e.preventDefault();
+    const form = e.target;
+
+    const fName = form.fName.value;
+    const lName = form.lName.value;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    const user = { fName, lName, email, password };
+
+    const upperCaseLetters = /[A-Z]/g;
+    const specialCharacter = /[!"#$%&'()*+,-.:;<=>?@[\]^_`{|}~]/;
+
+    // Validate capital letters
+    if (password.length < 6) {
+      Swal.fire("Password Must be 6 Character!");
+    }
+    else if (!password.match(upperCaseLetters)) {
+      Swal.fire("You must use a capital letter!");
+    }
+    else if (!password.match(specialCharacter)) {
+      Swal.fire("You must use a spectial character!");
+    }
+
+    if (password.length > 6 && password.match(upperCaseLetters) && password.match(specialCharacter)) {
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Your Registretion Successfull",
+        showConfirmButton: false,
+        timer: 1500
+      });
+    }
+
+
+    console.log(user);
+  }
+
+
+
   return (
     <div className="hero min-h-screen bg-slate-200">
       <div className="card w-full bg-white max-w-sm shrink-0 shadow-2xl">
-        <form className="card-body  ">
+        <form onSubmit={handleRegister} className="card-body  ">
           <div className="form-control">
+            <label className="label">
+              <span className="label-text text-black font-semibold">First Name</span>
+            </label>
+            <input type="text" name="fName" placeholder="First Name" className="input input-bordered text-white" required />
+
+            <label className="label">
+              <span className="label-text text-black font-semibold">Last Name</span>
+            </label>
+            <input type="text" name="lName" placeholder="Last Name" className="input input-bordered text-white" required />
+
             <label className="label">
               <span className="label-text text-black font-semibold">Email</span>
             </label>
-            <input type="email" placeholder="email" className="input input-bordered text-white" required />
-          </div>
-          <div className="form-control">
+            <input type="email" name="email" placeholder="Email" className="input input-bordered text-white" required />
+
             <label className="label">
               <span className="label-text text-black font-semibold">Password</span>
             </label>
-            <input type="password" placeholder="password" className="input input-bordered text-white" required />
-            
+            <input type="password" name="password" placeholder="Password" className="input input-bordered text-white" required id="pass" />
+
           </div>
           <div className="form-control mt-6">
             <button className="btn">Register</button>
